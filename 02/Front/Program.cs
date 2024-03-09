c98010c19e5ed900de01a2a8f46c74f88ba33029
dawnchan030920 <dawnchan030920@outlook.com>
2024-03-09 15:59:55 +0000

    [upd] bootstrap radzen and fluxor

--- 02/Front/Program.cs
+++ 02/Front/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Front;
+using Fluxor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddFluxor(options => options.ScanAssemblies(typeof(Program).Assembly));
 
 await builder.Build().RunAsync();
