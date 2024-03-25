3c1674cee868e3201b3b7539db83ef7b8690d0f4
dawnchan030920 <dawnchan030920@outlook.com>
2024-03-25 14:49:47 +0000

    [fix] fix format

--- 04/Clock/Program.cs
+++ 04/Clock/Program.cs
@@ -4,56 +4,56 @@ namespace Clock;
 
 class Program
 {
-	public static int Main(string[] args)
-	{
-		var rootCommand = new RootCommand("Clock simulation");
+    public static int Main(string[] args)
+    {
+        var rootCommand = new RootCommand("Clock simulation");
 
         var tickSpanOption = new Option<int>(
             name: "--span",
             description: "Ticking interval span in seconds",
             getDefaultValue: () => 1
         );
-						
+
         var tickCommand = new Command("tick", "Ticking every time")
-			{
+            {
                 tickSpanOption
             };
 
-		tickCommand.SetHandler((span) =>
-			{
-				var clock = new Clock();
-				clock.Tick += (time) => Console.WriteLine(time);
-				clock.StartTick(TimeSpan.FromSeconds(span));
+        tickCommand.SetHandler((span) =>
+            {
+                var clock = new Clock();
+                clock.Tick += (time) => Console.WriteLine(time);
+                clock.StartTick(TimeSpan.FromSeconds(span));
 
-				Console.ReadKey();
-			},
-			tickSpanOption);
+                Console.ReadKey();
+            },
+            tickSpanOption);
 
-		var alarmSpanOption = new Option<int>(
-			name: "--span",
-			description: "Alarming after a timespan in seconds",
-			getDefaultValue: () => 5
-		);
+        var alarmSpanOption = new Option<int>(
+            name: "--span",
+            description: "Alarming after a timespan in seconds",
+            getDefaultValue: () => 5
+        );
 
-		var alarmCommand = new Command("alarm", "Alarming after some time")
-			{
-				alarmSpanOption
-			};
+        var alarmCommand = new Command("alarm", "Alarming after some time")
+            {
+                alarmSpanOption
+            };
 
-		alarmCommand.SetHandler((span) =>
-			{
-				var clock = new Clock();
-				clock.Alarm += (time) => Console.WriteLine($"Time is up at {time}");
-				Console.WriteLine($"Alarm starts at {DateTime.Now}");
-				clock.StartAlarm(TimeSpan.FromSeconds(span));
+        alarmCommand.SetHandler((span) =>
+            {
+                var clock = new Clock();
+                clock.Alarm += (time) => Console.WriteLine($"Time is up at {time}");
+                Console.WriteLine($"Alarm starts at {DateTime.Now}");
+                clock.StartAlarm(TimeSpan.FromSeconds(span));
 
-				Console.ReadKey();
-			},
-			alarmSpanOption);
+                Console.ReadKey();
+            },
+            alarmSpanOption);
 
-		rootCommand.AddCommand(tickCommand);
-		rootCommand.AddCommand(alarmCommand);
+        rootCommand.AddCommand(tickCommand);
+        rootCommand.AddCommand(alarmCommand);
 
-		return rootCommand.Invoke(args);
-	}
+        return rootCommand.Invoke(args);
+    }
 }
