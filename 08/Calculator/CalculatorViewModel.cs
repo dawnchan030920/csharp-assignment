cc205b2035a7783a351df3d0b4e92ace9ef1965a
dawnchan030920 <dawnchan030920@outlook.com>
2024-04-23 11:56:13 +0000

    [add] add assignment 08

---
+++ 08/Calculator/CalculatorViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NCalc;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Calculator
+{
+    public partial class CalculatorViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(Result))]
+        private string? _expression;
+
+        public string Result => Expression == null ? "" : (new Expression(Expression)).Evaluate().ToString() ?? "";
+
+        [RelayCommand]
+        private void Number(string number)
+        {
+            Expression = $"{Expression}{number}";
+        }
+
+        [RelayCommand]
+        private void Operator(string op)
+        {
+            Expression = $"{Expression}{op}";
+        }
+
+        [RelayCommand]
+        private void Del()
+        {
+            if (Expression != null && Expression.Length > 0)
+            {
+                Expression = Expression.Substring(0, Expression.Length - 1);
+            }
+        }
+
+        [RelayCommand]
+        private void Clear()
+        {
+            Expression = "";
+        }
+    }
+}
---
+++ 08/README.md
@@ -0,0 +1,13 @@
+# C# Assignment 08
+
+## Introduction
+
+Implement a calculator.
+
+## Screenshots
+
+![calc](./screenshots/calc.png)
+
+![inf](./screenshots/inf.png)
+
+![nan](./screenshots/nan.png)
