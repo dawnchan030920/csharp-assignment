4e9328cb273114d8f80b55cf47041e37ad0bed6e
dawnchan030920 <dawnchan030920@outlook.com>
2024-05-06 14:48:34 +0000

    [add] assignment 09

---
+++ 09/README.md
@@ -0,0 +1,9 @@
+# Assignment 09
+
+## Introduction
+
+Parse emails and phone numbers in a given url.
+
+## Screenshots
+
+![result](./screenshots/result.png)
---
+++ 09/spider/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace PhoneEmailSpider;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty] private string _url;
+
+    public ObservableCollection<string> Emails { get; } = new();
+
+    public ObservableCollection<string> Phones { get; } = new();
+
+    [RelayCommand]
+    private async Task GetPhonesAndEmails()
+    {
+        using var client = new HttpClient();
+        var content = await client.GetStringAsync(Url);
+
+        var emailPattern = @"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"; // email with @ and domain
+        var emailMatches = Regex.Matches(content, emailPattern);
+        Emails.Clear();
+        foreach (Match match in emailMatches) Emails.Add(match.Value);
+
+        var phonePattern = @"\b\d{3}[-.]?\d{4}[-.]?\d{4}\b"; // 11 decimal digits with - or . separator
+        var phoneMatches = Regex.Matches(content, phonePattern);
+        Phones.Clear();
+        foreach (Match match in phoneMatches) Phones.Add(match.Value);
+    }
+}
