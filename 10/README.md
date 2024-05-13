4212b15a9c9467a862bc0735bddff4d6c8b31eff
dawnchan030920 <dawnchan030920@outlook.com>
2024-05-13 17:25:23 +0000

    [add] assignment 10

---
+++ 10/README.md
@@ -0,0 +1,16 @@
+# Assignment 10
+
+## Introduction
+
+Write a search engine client using Bing and/or Baidu to search.
+
+## Attention
+
+1. Selector is unfeasible to determine due to the complex and inconsistent structure of modern search engine page.
+2. Baidu has anti-spider mechanism.
+3. Register for API can cost money and is hence not adopted. Spider is in place of that.
+
+## Screenshots
+
+![whu](./screenshots/whu.png)
+![no-internet](./screenshots/no-internet.png)
---
+++ 10/SearchEngine/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System.Net.Http;
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using HtmlAgilityPack;
+
+namespace SearchEngine;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string? _searchText;
+
+    [ObservableProperty]
+    private string? _baiduResult;
+
+    [ObservableProperty]
+    private string? _bingResult;
+
+    [RelayCommand]
+    private async Task Search()
+    {
+        await SearchBaidu();
+        await SearchBing();
+    }
+    
+    private async Task SearchBaidu()
+    {
+        using var client = new HttpClient();
+        var response = await client.GetAsync("https://www.baidu.com/s?wd=" + SearchText);
+        BaiduResult = GetTwoHundredContent(await response.Content.ReadAsStringAsync());
+    }
+    
+    private async Task SearchBing()
+    {
+        using var client = new HttpClient();
+        var response = await client.GetAsync("https://www.bing.com/search?q=" + SearchText);
+        BingResult = GetTwoHundredContent(await response.Content.ReadAsStringAsync());
+    }
+    
+    private static string GetTwoHundredContent(string html)
+    {
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+        var paragraph = doc.DocumentNode.SelectNodes("//p");
+        var sb = new StringBuilder();
+        foreach (var p in paragraph)
+        {
+            sb.Append(p.InnerText);
+            if (sb.Length > 200)
+            {
+                break;
+            }
+        }
+        return sb.ToString();
+    }
+}
